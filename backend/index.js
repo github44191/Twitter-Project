9a341d6cfe61be3d6bbdb753ffb5371b6a523280
github44191 <manikandansatheesh10@gmail.com>
2024-07-15 00:08:04 +0530

    env changed

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s7oe3nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://manikandansatheesh2004:kd1VngjRzYniLG0N@cluster0.s7oe3nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
